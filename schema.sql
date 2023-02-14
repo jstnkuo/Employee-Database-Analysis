d53331b4e5383255f76a6368bb4f819794bd48fa
Justin Kuo <justinwkuo@gmail.com>
2023-02-13 23:54:28 -0800

    push schema.sql

---
+++ schema.sql
@@ -0,0 +1,44 @@
+CREATE TABLE employee(
+	emp_no INT NOT NULL,
+	emp_title_id VARCHAR(20) NOT NULL,
+	birth_date DATE,
+	first_name VARCHAR(20) NOT NULL,
+	last_name VARCHAR(20) NOT NULL,
+	sex VARCHAR(1),
+	hire_date DATE,
+	PRIMARY KEY (emp_no)
+	);
+	
+CREATE TABLE salaries(
+	emp_no INT NOT NULL,
+	salary INT NOT NULL,
+	FOREIGN KEY(emp_no) REFERENCES employee(emp_no),
+	PRIMARY KEY(emp_no)
+	);
+	
+CREATE TABLE department(
+	dept_no VARCHAR(10) NOT NULL,
+	dept_name VARCHAR(30) NOT NULL,
+	PRIMARY KEY(dept_no)
+	);	
+	
+CREATE TABLE dept_emp(
+	emp_no INT NOT NULL,
+	dept_no VARCHAR(10),
+	FOREIGN KEY(emp_no) REFERENCES employee(emp_no),
+	FOREIGN KEY(dept_no) REFERENCES department(dept_no)
+	);
+
+CREATE TABLE dept_manager(
+	dept_no VARCHAR(10) NOT NULL,
+	emp_no INT NOT NULL,
+	FOREIGN KEY(emp_no) REFERENCES employee(emp_no),
+	FOREIGN KEY(dept_no) REFERENCES department(dept_no)
+	);
+
+	
+CREATE TABLE titles(
+	title_id VARCHAR(10),
+	title VARCHAR(30),
+	PRIMARY KEY(title_id)
+	)
